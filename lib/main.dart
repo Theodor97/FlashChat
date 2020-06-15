0e08913a92e404257e8feb1b4c700fddbef10616
Theodor97 <60326175+Theodor97@users.noreply.github.com>
2020-06-15 12:59:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'screens/chat_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
+import 'screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
 
